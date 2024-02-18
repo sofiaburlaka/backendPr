949eb39df8e229f16eb6ab234f008e236a863910
Sofia Burlaka <sofiaburlaka@gmail.com>
2024-02-18 14:18:00 +0200

    Initial commit

---
+++ src/main/java/com/example/backendpr/BackendprApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendpr/BackendprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
